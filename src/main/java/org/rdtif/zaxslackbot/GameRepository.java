24d703d4e64cf5311080dd6d703b9a202614c99b
Iain Davis <iaindavis@carfax.com>
2016-12-20 13:11:58 -0600

    Implemented method for pulling back a list of z-machine files.

--- src/main/java/org/rdtif/zaxslackbot/GameRepository.java
+++ src/main/java/org/rdtif/zaxslackbot/GameRepository.java
@@ -1,13 +1,32 @@
 package org.rdtif.zaxslackbot;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import javax.inject.Inject;
 
 class GameRepository {
-    public GameRepository(ZaxSlackBotConfiguration configuration) {
+    private static final Predicate<Path> FOR_REGULAR_FILES = (path -> Files.isRegularFile(path));
+    private static final Predicate<String> ENDS_WITH_Z_VERSION = Pattern.compile(".*\\.z[1-5|7-8]$").asPredicate();
+    private final ZaxSlackBotConfiguration configuration;
 
+    @Inject
+    GameRepository(ZaxSlackBotConfiguration configuration) {
+        this.configuration = configuration;
     }
 
-    Stream<String> fileNames() {
-        return null;
+    Collection<String> fileNames() {
+        try (Stream<Path> paths = Files.walk(Paths.get(configuration.getGameDirectory()))) {
+            return paths.filter(FOR_REGULAR_FILES).map(Path::getFileName).map(Path::toString).filter(ENDS_WITH_Z_VERSION).collect(Collectors.toList());
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
+
 }
--- src/test/java/org/rdtif/zaxslackbot/GameRepositoryTest.java
+++ src/test/java/org/rdtif/zaxslackbot/GameRepositoryTest.java
@@ -1,19 +1,27 @@
 package org.rdtif.zaxslackbot;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
-import java.util.stream.Stream;
+import java.util.Random;
 
+@RunWith(Theories.class)
 public class GameRepositoryTest {
     @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
@@ -21,23 +29,59 @@ public class GameRepositoryTest {
     public void fileNamesNeverReturnsNull() {
         GameRepository repository = new GameRepository(createConfiguration());
 
-        Stream<String> stream = repository.fileNames();
+        Collection<String> stream = repository.fileNames();
 
         assertThat(stream, notNullValue());
     }
 
     @Test
-    public void fileNamesContainsAtLeastOneFile() {
+    public void fileNamesContainsOneFile() {
         GameRepository repository = new GameRepository(createConfiguration());
-        String fileName = RandomStringUtils.randomAlphanumeric(25);
-        createFile(fileName);
-        Stream<String> stream = repository.fileNames();
-        assertThat(stream.anyMatch(name -> name.equals(fileName)), equalTo(true));
+        String fileName = RandomStringUtils.randomAlphanumeric(25) + ".z8";
+        createFile(temporaryFolder.getRoot(), fileName);
+        Collection<String> names = repository.fileNames();
+        assertThat(names, hasItem(fileName));
     }
 
-    private File createFile(String fileName) {
+    @Theory
+    public void onlyValidExtensions(Integer version) {
+        GameRepository repository = new GameRepository(createConfiguration());
+        String fileName = RandomStringUtils.randomAlphanumeric(5) + ".z" + version;
+        String otherFileName = RandomStringUtils.randomAlphanumeric(5) + ".txt";
+        createFile(temporaryFolder.getRoot(), fileName);
+        createFile(temporaryFolder.getRoot(), otherFileName);
+        Collection<String> names = repository.fileNames();
+        assertThat(names, hasItem(fileName));
+        assertThat(names, not(hasItem(otherFileName)));
+    }
+
+    @DataPoints public static Collection<Integer> datapoints() {
+        return Arrays.asList(1,2,3,4,5,7,8);
+    }
+
+    @Test
+    public void includeSubDirectories() {
+        GameRepository repository = new GameRepository(createConfiguration());
+        String fileName = RandomStringUtils.randomAlphanumeric(25) + ".z8";
+        createFile(createFolder(), fileName);
+        Collection<String> names = repository.fileNames();
+        assertThat(names, hasItem(fileName));
+    }
+
+    private File createFolder() {
         try {
-            return temporaryFolder.newFile(fileName);
+            return temporaryFolder.newFolder();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private void createFile(File folder, String fileName) {
+        try {
+            boolean newFile = new File(folder, fileName).createNewFile();
+            if (!newFile) {
+                throw new RuntimeException();
+            }
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
