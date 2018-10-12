40504955b446c0fd57cb1bd04499b5343a2dd9a1
Iain Davis <iaindavis@carfax.com>
2018-10-12 11:53:03 -0500

    Last bit of conversion to junit5

--- src/test/java/org/rdtif/zaxslackbot/interpreter/LanguageProcessorTest.java
+++ src/test/java/org/rdtif/zaxslackbot/interpreter/LanguageProcessorTest.java
@@ -1,7 +1,7 @@
 package org.rdtif.zaxslackbot.interpreter;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 import java.util.Collections;
